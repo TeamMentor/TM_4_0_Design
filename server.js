f83aad3cf557545f6bafac86959d807cc7f44296
Dinis Cruz <dinis.cruz@owasp.org>
2014-09-04 23:56:38 +0100

    Added Express dependency and expanded Server.js to test express in Azure

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) { res.send("Using Express") });
+
+app.listen(port)
+
+console.log("Starting 'TM Jade' Poc on port " + port); 
