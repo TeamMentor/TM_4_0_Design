a523608484d5682e8388986667f9be082cc022b5
Dinis Cruz <dinis.cruz@owasp.org>
2014-09-05 03:24:13 +0100

    Refactored routes into separate file

--- server.js
+++ server.js
@@ -5,26 +5,8 @@ var express = require('express');
 
 var app = express();
 
-var sourceDir = '../source';  
+require('./node_tm/routes.js')(app);
 
-app.get('/getting-started/index.html', function (req, res)  { res.redirect('/user/returning-user-login.html')});
-
-//Redirect to Jade pages
-app.get('/'                            ,function (req, res)  { res.redirect('/default.html'                                                     );});
-app.get('/deploy/html/:area/:page.html',function (req, res)  { res.redirect('/'                 + req.params.area +'/'+req.params.page + '.html');}); 
-
-//Render Jade pages
-app.get('/:page.html'                  ,function (req, res)  { res.render  (sourceDir + '/html/'+ req.params.page                      + '.jade');}); 
-app.get('/:area/:page.html'            ,function (req, res)  { res.render  (sourceDir + '/html/'+ req.params.area +'/'+req.params.page + '.jade');}); 
-
-//Post data
-app.post('/action/login'               ,function (req, res)  
-        { 
-            res.redirect('/user/returning-user-validation.html');
-            
-            //res.redirect('/user/returning-user-forgot-password.html');
-        });
-app.get('/test', function(req,res) { res.send(__dirname) } );
 app.use(express.static(__dirname)); 
 
 app.listen(port)
