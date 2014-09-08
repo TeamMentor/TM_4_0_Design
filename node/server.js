a7dbd9666922852270a622b358bc1a290ba5c649
Dinis Cruz <dinis.cruz@owasp.org>
2014-09-08 17:09:06 +0100

    Added temp route to show module and mainModule contents To help debugging Azure problem

--- node/server.js
+++ node/server.js
@@ -2,7 +2,8 @@
 "use strict";
 
 var express    = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    util = require('util');
 
 var app      = express();
 app.use(bodyParser.json()                        );     // to support JSON-encoded bodies
@@ -10,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: true }));     // to support URL-encode
 //require('./routes/routes.js')(app);
 //require('./routes/user.js')(app);
 
+
+app.use('/module',function(req,res) { res.send(util.inspect(module));});
+app.use('/mainModule',function(req,res) { res.send(util.inspect(process.mainModule));});
 app.use('/',function(req,res) { res.send('simple azure test');});
 //app.use(express['static'](process.cwd()));
 
@@ -19,9 +23,12 @@ app.port       = process.env.PORT || 1337;
 if (process.mainModule === module)                                  // with this express will not start when this script is loaded from a UnitTest
 {
     console.log("Starting 'TM Jade' Poc on port " + app.port);
- //   app.listen(app.port);
+    app.listen(app.port);
 }
-
-app.listen(app.port);
+else
+{
+    console.log('for azure staring on port ' + app.port);
+    app.listen(app.port);           
+}
 
 module.exports = app;
